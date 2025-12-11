13a4e2cf791ec3d2d2284fec5d57e189a66d4ed8
Angie Moya <angiemoyau.1998@gmail.com>
2025-12-11 14:03:10 -0500

    product-page implemented

--- src/app/store-front/pages/product-page/product-page.ts
+++ src/app/store-front/pages/product-page/product-page.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { ProductsService } from '@products/services/products.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-page',
   imports: [],
   templateUrl: './product-page.html',
 })
-export class ProductPage { }
+export class ProductPage {
+  activatedRoute = inject(ActivatedRoute); // this line was omit in my try
+  productService = inject(ProductsService);
+
+  productIdSlug = this.activatedRoute.snapshot.params['idSlug']; // this line was omit in my try
+
+  productResource = rxResource({
+    params: () => ({ idSlug: this.productIdSlug }),
+    stream: ({ params }) => {
+      console.log(params);
+      return this.productService.getProductByIdSlug(params.idSlug);
+    },
+  });
+}
