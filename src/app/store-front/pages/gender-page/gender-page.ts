a7546f71d944659827e8742465057227afbc035a
Angie Moya <angiemoyau.1998@gmail.com>
2025-12-12 13:09:59 -0500

    display products by gender

--- src/app/store-front/pages/gender-page/gender-page.ts
+++ src/app/store-front/pages/gender-page/gender-page.ts
@@ -1,8 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { rxResource, toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { ProductCard } from '@products/components/product-card/product-card';
+import { ProductsService } from '@products/services/products.service';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-gender-page',
-  imports: [],
+  imports: [ProductCard],
   templateUrl: './gender-page.html',
 })
-export class GenderPage { }
+export class GenderPage {
+  route = inject(ActivatedRoute);
+  gender = toSignal(this.route.params.pipe(map(({ gender }) => gender)));
+
+  productsService = inject(ProductsService);
+
+  productsResource = rxResource({
+    params: () => ({ gender: this.gender() }),
+    stream: ({ params }) => {
+      return this.productsService.getProducts({ gender: params.gender });
+    },
+  });
+}
